860b24227a8aca1ccfc2c177c061263858dd57a2
kapilji <kapil707sharma@gmail.com>
2024-11-26 17:10:09 +0530

    add

--- socketHandler.js
+++ socketHandler.js
@@ -1,4 +1,22 @@
 const chatModel = require("./models/chatModel");
+const admin = require("./firebase"); // Import initialized Firebase Admin
+
+async function sendNotificationToDevice(token, title, body) {
+  const message = {
+    notification: {
+      title: title,
+      body: body,
+    },
+    token: token, // Firebase token of the target device
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log("Notification sent successfully:", response);
+  } catch (error) {
+    console.error("Error sending notification:", error);
+  }
+}
 
 const users = {}; // Keep track of connected users
 
