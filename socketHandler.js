bd4697529e0ee6793ae3b5c0a0263adda33891dd
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-11-28 16:05:49 +0530

    add

--- socketHandler.js
+++ socketHandler.js
@@ -123,9 +123,9 @@ function initializeSocket(io) {
 
     // Handle message sending
     socket.on("userTyping", async ({ user1, user2, status }) => {
-      console.log('userTyping');
         const recipientSocketIds = users[user2];
         if (recipientSocketIds) {
+          console.log('userTyping');
           io.to(recipientSocketIds).emit("receiveTyping", {
             status:status
           });
