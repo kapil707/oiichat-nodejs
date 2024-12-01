51d45439fa41479ba3519ea7f43b55e5418a5b07
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-12-01 11:46:41 +0530

    add

--- socketHandler.js
+++ socketHandler.js
@@ -145,7 +145,7 @@ function initializeSocket(io) {
     //get old message
     socket.on("get_old_message", async (user_id) => {
 
-      const old_message = await userModel.findOne({ user2: user_id });
+      const old_message = await chatModel.findOne({ user2: user_id });
 
       socket.emit('get_old_message_response', {
         message: old_message.message
