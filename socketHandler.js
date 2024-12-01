86b6a165177ecaaf698b7e1df63138187ad43c6a
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-12-01 14:57:01 +0530

    add

--- socketHandler.js
+++ socketHandler.js
@@ -149,6 +149,9 @@ function initializeSocket(io) {
     
         // Fetch all messages where user2 matches the given user_id
         const old_messages = await chatModel.find({ user2: user_id,status:0 });
+
+        //update message status
+        await chatModel.updateMany({ user2: user_id, status: 0 }, { $set: { status: 1 } });
     
         // Send the messages back to the client
         socket.emit('get_old_message_response', {
