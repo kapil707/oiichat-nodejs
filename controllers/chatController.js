64b8c0e274a6eb6606d3b6ca4b7d6dd26453d1d1
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-12-01 16:44:00 +0530

    add

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -22,7 +22,19 @@ async function fetchOldMessages(req,res) {
   console.log("Converted user_id:", new mongoose.Types.ObjectId(user_id));
 
   try {
-    const old_messages = await chatModel.find({ user2: user_id});
+    const old_messages = await chatModel.aggregate([
+      {
+        $match: { user2: user_id, status: 0 },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "user1",
+          foreignField: "_id",
+          as: "user1_info",
+        },
+      },
+    ]);
 
 
     console.log("Old Messages with User Info:", old_messages);
