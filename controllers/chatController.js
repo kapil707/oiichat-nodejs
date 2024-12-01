fd977c9bc11de8b3822f5ebf9b94c4e75af0f025
kapil707 <kapilsharma@kapils-MacBook-Air.local>
2024-12-01 16:10:29 +0530

    add

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -10,11 +10,40 @@ async function getAllUser(req,res) {
 }
 
 async function fetchOldMessages(req,res) {
-  const users = await userModel.find({});
-  return res.status(200).send({ 
-    status: 1,
-    message: 'data load successful!', 
-    users});
+  // const users = await userModel.find({});
+  // return res.status(200).send({ 
+  //   status: 1,
+  //   message: 'data load successful!', 
+  //   users});
+
+  try {
+    const old_messages = await chatModel.aggregate([
+      {
+        $match: { 
+          user2: user_id, 
+          status: 0 
+        },
+      },
+      {
+        $lookup: {
+          from: "users", // Ensure this matches your collection name
+          localField: "user1",
+          foreignField: "_id",
+          as: "user1_info",
+        },
+      },
+      {
+        $unwind: "$user1_info",
+      },
+    ]);
+
+    console.log("Old Messages with User Info:", old_messages);
+
+    return res.status(200).send({ old_messages });
+  } catch (error) {
+    console.error("Error fetching messages:", error.message);
+    return res.status(200).send({ error });
+  }
 }
 
 module.exports = {
